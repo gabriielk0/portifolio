42299068bbcb188f56092028cee7f545318409e3
Gabriel <oliveiracgabriel052@gmail.com>
2024-10-01 21:26:42 -0300

    Adicionando elementos na Sidebar

--- src/components/Slidebar/index.jsx
+++ src/components/Slidebar/index.jsx
@@ -3,35 +3,36 @@ import styles from './Sidebar.module.css'
 
 export default function Sidebar() {
   return (
-    <aside>
+    <aside className={styles.aside}>
       <img src="http://github.com/gabriielk0.png" alt="Logo do CodeConnect" className={styles.imagem}/>
       <nav>
-        <ul className="lista-sidebar">
+        <ul className={styles.lista_sidebar}>
+          <h1 className={styles.textoPrincipal}>Gabriel de Carvalho Oliveira</h1>
           <li>
             <a href="#" className={styles.item__link}>
               Publicar
             </a>
           </li>
           <li>
-            <a href="#" className="item__link item__link--ativo">
+            <a href="#" className={styles.item__link}>
               <img src="{Feed}" alt="" />
               <span>Feed</span>
             </a>
           </li>
           <li>
-            <a href="#" className="item__link">
+            <a href="#" className={styles.item__link}>
               <img src="{Account}" alt="" />
               <span>Perfil</span>
             </a>
           </li>
           <li>
-            <a href="#" className="item__link">
+            <a href="#" className={styles.item__link}>
               <img src="{Info}" alt="" />
               <span>Sobre Nós</span>
             </a>
           </li>
           <li>
-            <a href="#" className="item__link">
+            <a href="#" className={styles.item__link}>
               <img src="{Logout}" alt="" />
               <span>Sair</span>
             </a>
