a6b9b7514c5a46cf1cf2626523be605b5b924563
Gabriel <oliveiracgabriel052@gmail.com>
2024-10-01 22:52:58 -0300

    Adicionando componentes na Sidebar

--- src/components/Slidebar/index.jsx
+++ src/components/Slidebar/index.jsx
@@ -1,34 +1,38 @@
 
 import styles from './Sidebar.module.css'
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 export default function Sidebar() {
   return (
     <aside className={styles.aside}>
       <img src="http://github.com/gabriielk0.png" alt="Logo do CodeConnect" className={styles.imagem}/>
+      <h1 className={styles.textoPrincipal}>Gabriel de Carvalho Oliveira</h1>
+      <p className={styles.textoSecundario}>
+        Olá! Sou Gabriel, estudante de Ciências da Computação. 
+        Bem-vindo ao meu portfólio! Aqui você vai encontrar meus projetos, 
+        criados com muito entusiasmo e dedicação.
+        </p>
+        <hr/>
       <nav>
         <ul className={styles.lista_sidebar}>
-          <h1 className={styles.textoPrincipal}>Gabriel de Carvalho Oliveira</h1>
+          <h3 className={styles.textoPrincipal}>Redes Sociais</h3>
+          
           <li>
             <a href="#" className={styles.item__link}>
-              Publicar
+              <FaGithub />
+              <span>Github</span>
             </a>
           </li>
           <li>
             <a href="#" className={styles.item__link}>
-              <img src="{Feed}" alt="" />
-              <span>Feed</span>
+              <FaLinkedin/>
+              <span>Linkedin</span>
             </a>
           </li>
           <li>
             <a href="#" className={styles.item__link}>
-              <img src="{Account}" alt="" />
-              <span>Perfil</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" className={styles.item__link}>
-              <img src="{Info}" alt="" />
-              <span>Sobre Nós</span>
+              <FaInstagram/>
+              <span>Instagram</span>
             </a>
           </li>
           <li>
