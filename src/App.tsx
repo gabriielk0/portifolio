df8e2354a10dc7ce71e238ab30a22db1d08f63ec
Gabriel <oliveiracgabriel052@gmail.com>
2026-02-16 00:30:26 -0300

    atualização na ordem dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,7 +18,6 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,9 +13,9 @@ const Index = () => {
       <Navbar />
       <main>
         <Hero />
-        <Career />
-        <Skills />
         <Projects />
+        <Skills />
+        <Career />
       </main>
       <Footer />
     </div>
