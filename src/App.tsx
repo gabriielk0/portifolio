e7ccc5ecbc735b7160a0a8f121e7935669f24b85
Gabriel <oliveiracgabriel052@gmail.com>
2026-02-17 12:52:44 -0300

    alterando index

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { ThemeProvider } from "next-themes";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import { ThemeProvider } from 'next-themes';
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Home/index';
+import NotFound from './pages/NotFound';
 
 const queryClient = new QueryClient();
 
